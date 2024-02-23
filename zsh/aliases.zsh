3160dc4de7b66f621f13cd67c849616be720f950
Marcelo Adamatti <marcelo.adamatti@hotelengine.com>
2024-02-23 10:54:48 -0300

    chore: remove gupa alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,13 +4,14 @@ alias m="make"
 alias mm="make -f Makefile.adamatti"
 alias nvm="fnm"
 alias t="task"
-alias tf="terraform"
 alias zshconfig="code ~/.zshrc"
 alias zshedit="code ~/.zshrc"
+
+alias tf="terraform"
 alias tfl="tflint --disable-rule=terraform_unused_declarations --disable-rule=terraform_required_providers --disable-rule=terraform_required_version --disable-rule=terraform_deprecated_interpolation --recursive -f compact"
 
 # oh-my-zsh / git plugin deprecated
-alias gupa="git pull --autostash"
+# alias gupa="git pull --autostash"
 
 # Pet
 alias pes="pet search --color"
