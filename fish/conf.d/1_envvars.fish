494b6d550b841e1e12f5124460cd6ebb451687d4
Marcelo Adamatti <adamatti@gmail.com>
2025-11-13 11:42:59 -0300

    chore: remove zsh refs

--- fish/conf.d/1_envvars.fish
+++ fish/conf.d/1_envvars.fish
@@ -1,2 +1,12 @@
 set -Ux DOTFILES_ROOT "/Users/adamatti/workspace/github_ws/dotfiles"
-source $DOTFILES_ROOT/zsh/exports.zsh
+
+set -Ux HOMEBREW_NO_ENV_HINTS "true"
+set -Ux HOMEBREW_PREFIX "$(brew --prefix)"
+set -Ux NEXT_TELEMETRY_DISABLED "true"
+
+# https://martinheinz.dev/blog/110
+set -Ux HISTSIZE 10000000
+set -Ux SAVEHIST 10000000
+set -Ux HISTORY_IGNORE "(ls|pwd|exit|export AWS_)*"
+
+set -Ux VISUAL "nano"
