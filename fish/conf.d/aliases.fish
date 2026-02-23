1e2c7ca7c733db2b3fdb1c6ac6302158a9a19dd5
Marcelo Adamatti <adamatti@gmail.com>
2026-02-23 10:42:00 -0300

    fix: aliases

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -1,6 +1,2 @@
 alias ...="cd ../../"
 alias ....="cd ../../../"
-
-# Services
-alias dolar="http -p 'b' :8880/api/trpc/finance.getDollar | jq .result.data"
-alias dollar="http -p 'b' :8880/api/trpc/finance.getDollar | jq .result.data"
