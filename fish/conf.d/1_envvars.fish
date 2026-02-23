52971d9c2b117008e37c340af1cc825173b42b5d
Marcelo Adamatti <adamatti@gmail.com>
2026-02-23 10:39:15 -0300

    chore: env vars

--- fish/conf.d/1_envvars.fish
+++ fish/conf.d/1_envvars.fish
@@ -7,6 +7,8 @@ set -Ux NEXT_TELEMETRY_DISABLED "true"
 # https://martinheinz.dev/blog/110
 set -Ux HISTSIZE 10000000
 set -Ux SAVEHIST 10000000
-set -Ux HISTORY_IGNORE "(ls|pwd|exit|export AWS_)*"
+set -Ux HISTORY_IGNORE "(ls|pwd|exit|export AWS_|wezterm)*"
 
 set -Ux VISUAL "nano"
+
+set -Ux GRANTED_ALIAS_CONFIGURED "true"
